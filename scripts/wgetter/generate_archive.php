89cf1dcd1743ac732abc2c2c2bce8457a093a64a
Juks <juks@alkohol.ee>
2018-12-01 02:02:53 +0200

    dates data updated

--- scripts/wgetter/generate_archive.php
+++ scripts/wgetter/generate_archive.php
@@ -3,6 +3,7 @@
 $dirs_down = 2;
 include(dirname(__DIR__,$dirs_down).'/includes/web_init.php');
 if(!$init_data['dynamic']) exit;
+$arhiiv_id = 7;
 $fragm = [
     'header' => "<html>\n<head>\n<meta http-equiv=Content-Type content=\"text/html; charset=UTF-8\">\n<title>".'%1$s'."</title>\n<link rel=\"SHORTCUT ICON\" href=\"../favicon.ico\"/>\n<link rel=\"StyleSheet\" href=\"arhiiv.css\" type=\"text/css\"/>\n</head>\n<body>\n",
     'footer' => "</body>\n</html>",
@@ -10,7 +11,7 @@ $fragm = [
     'file' => 'e%1$05s.html',
     'dir' => dirname(__DIR__,$dirs_down).'/txt/'
 ];
-$arhiiv_id = 7;
+
 
 $dbh = new PDO('sqlite:'.dirname(__DIR__,$dirs_down).'/'.str_replace('sqlite:','', $init_data['dbc']));
 $events = array();
@@ -23,20 +24,22 @@ foreach ($dbh->query("select id, event, maausk from events") as $row) {
 
 $files = [];
 $previous_id = 0;
-$dbht = new PDO('sqlite:'.dirname(__DIR__,$dirs_down).'/kalender_texts.sqlite3');
-foreach ($dbht->query("select * from jae order by date, url") as $row) {
+$dbht = new PDO('sqlite:'.dirname(__DIR__,$dirs_down).'/archive.sqlite3');
+
+
+foreach ($dbht->query("select * from archive where description <> '' order by date, url") as $row) {
     $date = (int) $row['date'];
     if($date != $previous_id) {
         echo sprintf("%05d %s\n", $date, $events['E'.$date]);
         if($previous_id) {
-            file_put_contents($fragm['dir'].sprintf($fragm['file'],$previous_id), $file."\n<strong>Andmebaasitõmmis loodud ".date('Y-m-d H:i:s').'</strong>'.$fragm['footer']);
+            file_put_contents($fragm['dir'].sprintf($fragm['file'],$previous_id), $file."\n".$fragm['footer']);
         }
-        $file = sprintf($fragm['header'],$events['E'.$date]).'<h1>'.sprintf($fragm['main_title'],'index.html').': '.$events['E'.$date]."</h1>\n<hr/>\n";
+        $file = sprintf($fragm['header'],$events['E'.$date]).'<h1>'.sprintf($fragm['main_title'],'index.html').': '.$events['E'.$date]."</h1>\nUuendatud ".date('Y-m-d H:i:s')."\n\n";
         $files[] = $date;
         $previous_id = $date;
     }
     $hash = hash('sha256',$row['url']);
-    $file .= '<a name="'
+    $file .= '<hr/><a name="'
         .$hash
         .'"></a><h2><a href="#'
         .$hash
@@ -48,8 +51,20 @@ foreach ($dbht->query("select * from jae order by date, url") as $row) {
         .$row['url']
         ."\" target=\"_blank\">"
         .$row['url']
-        ."</a><br/><br/><hr/>\n";
+        ."</a><br/><br/>\n";
 }
+file_put_contents($fragm['dir'].sprintf($fragm['file'],$previous_id), $file."\n".$fragm['footer']);
+
+$sthu = $dbht->prepare("INSERT INTO dead_urls (id, urlcategory_id, event_id, url, flags, http_status) VALUES (?, ?, ?, ?, ?, ?)");
+$sthj = $dbht->prepare("INSERT INTO archive (date, description, newlines, url) VALUES (?, ?, ?, ?)");
+foreach ($dbh->query("SELECT * FROM urls WHERE http_status <> 200") as $row) {
+    //$sthu->execute([$row['id'], $row['urlcategory_id'], $row['event_id'], $row['url'], $row['flags'], $row['http_status']]);
+    //$sthj->execute([$row['event_id'], '', 2, 'https://web.archive.org/web/*/'.$row['url']]);
+}
+$dbh->query("DELETE FROM urls WHERE http_status <> 200");
+$sthu = null;
+$sthj = null;
+
 $dbht = null;
 
 $indexfile = sprintf($fragm['header'],'Eesti tähtpäevad').'<h1>'.sprintf($fragm['main_title'],'../tahtpaevad.html')."</h1>\n<ol>\n";
@@ -61,7 +76,7 @@ foreach($files as $id) {
     $result = $sthc->fetch(PDO::FETCH_ASSOC);
     if((int) $result['c'] == 0) { $sthi->execute([$arhiiv_id, $id, sprintf($fragm['file'],$id)]); }
 }
-file_put_contents($fragm['dir'].'index.html', $indexfile."</ol>\nAndmebaasitõmmis loodud ".date('Y-m-d H:i:s').$fragm['footer']);
+file_put_contents($fragm['dir'].'index.html', $indexfile."</ol>\nUuendatud ".date('Y-m-d H:i:s').$fragm['footer']);
 $sthc = null;
 $sthi = null;
 
